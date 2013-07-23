0914888919ff67cc0ccd93afa5a4b9f21517489a
Mike Walker <noisufnoc@gmail.com>
2013-07-22 21:40:59 -0700

    just need some facts, mam

---
+++ app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import random
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+facts = [
+    'bananas are delicious',
+    'i like turtles',
+    'jeff is a pug'
+]
+
+
+@app.route('/')
+def index():
+    return jsonify({'fact': facts[random.randint(0, 2)]})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
